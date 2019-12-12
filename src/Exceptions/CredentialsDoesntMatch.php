77e970ba743acf8932e0a7490980d5d8fb41b56f
Lupacescu Eduard <lupacescueduard@yahoo.com>
2019-12-12 18:31:53 +0200

    Apply fixes from StyleCI (#8)

--- src/Exceptions/CredentialsDoesntMatch.php
+++ src/Exceptions/CredentialsDoesntMatch.php
@@ -5,7 +5,6 @@ namespace Binaryk\LaravelRestify\Exceptions;
 use Exception;
 
 /**
- * @package App\Exceptions;
  * @author Eduard Lupacescu <eduard.lupacescu@binarcode.com>
  */
 class CredentialsDoesntMatch extends Exception
