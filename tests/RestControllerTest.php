fa2c0bd05d21c2bc17feed296b6560f9753f4ca2
Lupacescu Eduard <lupacescueduard@yahoo.com>
2019-12-15 14:21:59 +0200

    Apply fixes from StyleCI (#21)

--- tests/RestControllerTest.php
+++ tests/RestControllerTest.php
@@ -59,50 +59,51 @@ class RestControllerTest extends IntegrationTest
         $this->expectException(EntityNotFoundException::class);
         $this->controller->show(1);
     }
-/*
-    public function test_gate_restrict_access()
-    {
-        $user = factory(User::class)->create();
 
-        Gate::shouldReceive('check')
-            ->andReturnFalse();
-        $this->expectException(GatePolicy::class);
-        $this->expectExceptionMessage(__('messages.no_model_access'));
-        $this->controller->show($user->id);
-    }
-
-    public function test_gate_allow_access()
-    {
-        $user = factory(User::class)->create();
-        Gate::shouldReceive('check')
-            ->andReturnTrue();
-
-        $this->assertTrue($this->controller->gate('access', $user));
-        $response = $this->controller->show($user->id);
-        $this->assertEquals($user->email, data_get($response->getData(), 'data.email'));
-    }
-
-    public function test_making_custom_response()
-    {
-        $user = factory(User::class)->create();
-
-        Gate::shouldReceive('check')
-            ->andReturnFalse();
-        $response = $this->controller->update(new FormRequest(), $user->id);
-
-        $this->assertEquals($response->getStatusCode(), RestResponse::REST_RESPONSE_MISSING_CODE);
-        $this->assertSame($response->getData()->errors, ['Entity not found.']);
-    }
-
-    public function test_making_custom_response_message()
-    {
-        $user = factory(User::class)->create();
-
-        Gate::shouldReceive('check')
-            ->andReturnTrue();
-        $response = $this->controller->destroy($user->id);
-        $this->assertSame($response->getData()->message, 'User deleted.');
-    }*/
+    /*
+        public function test_gate_restrict_access()
+        {
+            $user = factory(User::class)->create();
+    
+            Gate::shouldReceive('check')
+                ->andReturnFalse();
+            $this->expectException(GatePolicy::class);
+            $this->expectExceptionMessage(__('messages.no_model_access'));
+            $this->controller->show($user->id);
+        }
+    
+        public function test_gate_allow_access()
+        {
+            $user = factory(User::class)->create();
+            Gate::shouldReceive('check')
+                ->andReturnTrue();
+    
+            $this->assertTrue($this->controller->gate('access', $user));
+            $response = $this->controller->show($user->id);
+            $this->assertEquals($user->email, data_get($response->getData(), 'data.email'));
+        }
+    
+        public function test_making_custom_response()
+        {
+            $user = factory(User::class)->create();
+    
+            Gate::shouldReceive('check')
+                ->andReturnFalse();
+            $response = $this->controller->update(new FormRequest(), $user->id);
+    
+            $this->assertEquals($response->getStatusCode(), RestResponse::REST_RESPONSE_MISSING_CODE);
+            $this->assertSame($response->getData()->errors, ['Entity not found.']);
+        }
+    
+        public function test_making_custom_response_message()
+        {
+            $user = factory(User::class)->create();
+    
+            Gate::shouldReceive('check')
+                ->andReturnTrue();
+            $response = $this->controller->destroy($user->id);
+            $this->assertSame($response->getData()->message, 'User deleted.');
+        }*/
 
     public function test_can_access_config_repository()
     {
