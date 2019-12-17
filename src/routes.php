a786735b2686d1c8f0253842c42fc92d8256ef69
Lupacescu Eduard <lupacescueduard@gmail.com>
2019-12-17 14:17:59 +0200

    Add basic mock routes

--- src/routes.php
+++ src/routes.php
@@ -1,1 +1,13 @@
 <?php
+
+Route::get('email/verify/{id}/{hash}', function($request) {
+    //Silence is golden
+})
+    ->name('register.verify')->middleware([
+    'signed',
+    'throttle:6,1',
+]);
+
+Route::post('password/reset', function () {
+    // Validate token
+})->name('password.reset');
