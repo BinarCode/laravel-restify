72ebdd866f5f0d86322edf4cd7825c2f8b35cca7
Lupacescu Eduard <lupacescueduard@yahoo.com>
2020-01-06 09:54:12 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/RepositoryDestroyController.php
+++ src/Http/Controllers/RepositoryDestroyController.php
@@ -2,7 +2,6 @@
 
 namespace Binaryk\LaravelRestify\Http\Controllers;
 
-use Binaryk\LaravelRestify\Controllers\RestResponse;
 use Binaryk\LaravelRestify\Exceptions\Eloquent\EntityNotFoundException;
 use Binaryk\LaravelRestify\Exceptions\UnauthorizedException;
 use Binaryk\LaravelRestify\Http\Requests\RepositoryDestroyRequest;
@@ -10,7 +9,6 @@ use Binaryk\LaravelRestify\Repositories\Repository;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 use Throwable;
 
 /**
--- src/Repositories/Crudable.php
+++ src/Repositories/Crudable.php
@@ -49,7 +49,7 @@ trait Crudable
         return (new static ($model))
             ->response()
             ->setStatusCode(RestResponse::REST_RESPONSE_CREATED_CODE)
-            ->header('Location', Restify::path() . '/' . self::uriKey() . '/' . $model->id);
+            ->header('Location', Restify::path().'/'.self::uriKey().'/'.$model->id);
     }
 
     /**
@@ -85,5 +85,4 @@ trait Crudable
         return $this->response()
             ->setStatusCode(RestResponse::REST_RESPONSE_DELETED_CODE);
     }
-
 }
