acdf4a310492e7af4e1e44b980c865cc342a5bdc
Lupacescu Eduard <lupacescueduard@yahoo.com>
2020-01-21 10:40:16 +0000

    Apply fixes from StyleCI

--- tests/Controllers/RepositoryShowControllerTest.php
+++ tests/Controllers/RepositoryShowControllerTest.php
@@ -3,9 +3,7 @@
 namespace Binaryk\LaravelRestify\Tests\Controllers;
 
 use Binaryk\LaravelRestify\Tests\Fixtures\Post;
-use Binaryk\LaravelRestify\Tests\Fixtures\PostPolicy;
 use Binaryk\LaravelRestify\Tests\IntegrationTest;
-use Illuminate\Support\Facades\Gate;
 
 /**
  * @author Eduard Lupacescu <eduard.lupacescu@binarcode.com>
@@ -20,7 +18,7 @@ class RepositoryShowControllerTest extends IntegrationTest
 
     public function test_basic_show()
     {
-        factory(Post::class)->create(['user_id' => 1,]);
+        factory(Post::class)->create(['user_id' => 1]);
 
         $this->withoutExceptionHandling()->get('/restify-api/posts/1')
             ->assertStatus(200)
