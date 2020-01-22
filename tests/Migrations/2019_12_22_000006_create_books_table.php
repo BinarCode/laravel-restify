1f357ba1ee7add93a00fa339c26d12f7dbf62882
Lupacescu Eduard <lupacescueduard@yahoo.com>
2020-01-22 09:23:53 +0000

    Apply fixes from StyleCI

--- tests/Migrations/2019_12_22_000006_create_books_table.php
+++ tests/Migrations/2019_12_22_000006_create_books_table.php
@@ -16,7 +16,7 @@ class CreateBooksTable extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->longText('description')->nullable();;
+            $table->longText('description')->nullable();
             $table->string('author')->nullable();
             $table->float('price')->default(0);
             $table->integer('stock')->nullable();
