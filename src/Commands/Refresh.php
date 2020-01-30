0fdf277a55c181b48711742ef3b79d0fb8ebde34
Lupacescu Eduard <lupacescueduard@yahoo.com>
2020-01-30 11:16:30 +0200

    Refresh command (#129)

---
+++ src/Commands/Refresh.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Binaryk\LaravelRestify\Commands;
+
+use Illuminate\Console\Command;
+
+class Refresh extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'restify:refresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear all laravel caches (routes, cache, config and view)';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->call('route:cache');
+        $this->call('cache:clear');
+        $this->call('config:cache');
+        $this->call('view:clear');
+        $this->call('optimize');
+    }
+}
