594c1910493c0e22b290e74cb7ee91073f9c8cb3
Lupacescu Eduard <lupacescueduard@gmail.com>
2020-05-07 20:37:51 +0000

    Apply fixes from StyleCI

--- src/Fields/FieldCollection.php
+++ src/Fields/FieldCollection.php
@@ -8,42 +8,42 @@ use Illuminate\Support\Collection;
 
 class FieldCollection extends Collection
 {
-    public function authorized(Request $request): FieldCollection
+    public function authorized(Request $request): self
     {
         return $this->filter(function (OrganicField $field) use ($request) {
             return $field->authorize($request);
         })->values();
     }
 
-    public function resolve($resource): FieldCollection
+    public function resolve($resource): self
     {
         return $this->each(function ($field) use ($resource) {
             $field->resolve($resource);
         });
     }
 
-    public function forIndex(RestifyRequest $request, $resource): FieldCollection
+    public function forIndex(RestifyRequest $request, $resource): self
     {
         return $this->filter(function (Field $field) use ($resource, $request) {
             return $field->isShownOnIndex($request, $resource);
         })->values();
     }
 
-    public function forShow(RestifyRequest $request, $resource): FieldCollection
+    public function forShow(RestifyRequest $request, $resource): self
     {
         return $this->filter(function (Field $field) use ($resource, $request) {
             return $field->isShownOnShow($request, $resource);
         })->values();
     }
 
-    public function forStore(RestifyRequest $request, $resource): FieldCollection
+    public function forStore(RestifyRequest $request, $resource): self
     {
         return $this->filter(function (Field $field) use ($resource, $request) {
             return $field->isShownOnStore($request, $resource);
         })->values();
     }
 
-    public function forUpdate(RestifyRequest $request, $resource): FieldCollection
+    public function forUpdate(RestifyRequest $request, $resource): self
     {
         return $this->filter(function (Field $field) use ($resource, $request) {
             return $field->isShownOnUpdate($request, $resource);
