324d73451c7ab779bcab4d8e7f6788f970b2da35
Eduard Lupacescu <lupacescueduard@gmail.com>
2020-05-09 20:23:34 +0300

    Clean up

--- tests/Fixtures/PostRepository.php
+++ tests/Fixtures/PostRepository.php
@@ -6,27 +6,10 @@ use Binaryk\LaravelRestify\Fields\Field;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Binaryk\LaravelRestify\Repositories\Repository;
 
-/**
- * @author Eduard Lupacescu <eduard.lupacescu@binarcode.com>
- */
 class PostRepository extends Repository
 {
     public static $model = Post::class;
 
-    /**
-     * Get the URI key for the resource.
-     *
-     * @return string
-     */
-    public static function uriKey()
-    {
-        return 'posts';
-    }
-
-    /**
-     * @param  RestifyRequest  $request
-     * @return array
-     */
     public function fields(RestifyRequest $request)
     {
         return [
