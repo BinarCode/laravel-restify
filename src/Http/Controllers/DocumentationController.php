59ce162ff053fe63326a0d98263b176dac889a0e
Eduard Lupacescu <lupacescueduard@gmail.com>
2020-06-18 22:58:52 +0300

    wip

---
+++ src/Http/Controllers/DocumentationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Binaryk\LaravelRestify\Http\Controllers;
+
+/**
+ * @package Binaryk\LaravelRestify\Http\Controllers;
+ * @author Eduard Lupacescu <eduard.lupacescu@binarcode.com>
+ */
+class DocumentationController
+{
+
+}
---
+++ tests/Controllers/DocumentationControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Binaryk\LaravelRestify\Tests\Controllers;
+
+/**
+ * @package Binaryk\LaravelRestify\Tests\Controllers;
+ * @author Eduard Lupacescu <eduard.lupacescu@binarcode.com>
+ */
+class DocumentationControllerTest
+{
+
+}
