605a2e10ba47be4df4af9fdd58ce3113b08325fe
Lupacescu Eduard <binaryk@users.noreply.github.com>
2020-08-13 12:48:00 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/RepositoryShowController.php
+++ src/Http/Controllers/RepositoryShowController.php
@@ -9,8 +9,8 @@ class RepositoryShowController extends RepositoryController
     public function __invoke(RepositoryShowRequest $request)
     {
         return $request->newRepositoryWith(tap(
-            tap($request->findModelQuery(), fn($query) => $request->repository()::mainQuery($request, $query)),
-            fn($query) => $request->repository()::showQuery($request, $query))->firstOrFail())
+            tap($request->findModelQuery(), fn ($query) => $request->repository()::mainQuery($request, $query)),
+            fn ($query) => $request->repository()::showQuery($request, $query))->firstOrFail())
             ->allowToShow($request)
             ->show($request, request('repositoryId'));
     }
