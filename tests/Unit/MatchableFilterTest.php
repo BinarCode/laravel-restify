4c237117de74bc900c192f3d5034333ce471e661
Eduard Lupacescu <lupacescueduard@gmail.com>
2021-05-27 14:58:02 +0300

    Ensure column visible test.

--- tests/Unit/MatchableFilterTest.php
+++ tests/Unit/MatchableFilterTest.php
@@ -11,13 +11,16 @@ class MatchableFilterTest extends IntegrationTest
     public function test_matchable_filter_has_key(): void
     {
         $filter = new class extends MatchFilter {
+            public ?string $column = 'status';
         };
 
         tap(
             AssertableJson::fromArray($filter->jsonSerialize()),
             function (AssertableJson $json) {
                 $json
+                    ->dump()
                     ->where('key', 'matches')
+                    ->where('column', 'status')
                 ;
             }
         );
