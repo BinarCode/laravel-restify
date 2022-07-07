08b54e6f2c29b48a4ad0532c23e4ecf2c062bc33
Eduard Lupacescu <eduard.lupacescu@binarcode.com>
2022-07-07 12:56:15 +0300

    fix: wip

--- src/Traits/AuthorizableModels.php
+++ src/Traits/AuthorizableModels.php
@@ -16,40 +16,11 @@ use Illuminate\Support\Facades\Gate;
  */
 trait AuthorizableModels
 {
-    /**
-     * Determine if the given resource is authorizable.
-     *
-     * @return bool
-     */
-    public static function authorizable()
+    public static function authorizable(): bool
     {
         return ! is_null(Gate::getPolicyFor(static::newModel()));
     }
 
-    /**
-     * Determine if the Restify is enabled for this repository.
-     *
-     * @param  Request  $request
-     * @return void
-     * @throws AuthorizationException
-     */
-    public function authorizeToUseRepository(Request $request)
-    {
-        if (! static::authorizable()) {
-            return;
-        }
-
-        if (method_exists(Gate::getPolicyFor(static::newModel()), 'allowRestify')) {
-            $this->authorizeTo($request, 'allowRestify');
-        }
-    }
-
-    /**
-     * Determine if the repository should be available for the given request.
-     *
-     * @param  Request  $request
-     * @return bool
-     */
     public static function authorizedToUseRepository(Request $request): bool
     {
         if (! static::authorizable()) {
@@ -62,56 +33,39 @@ trait AuthorizableModels
     }
 
     /**
-     * Determine if the current user can view the given resource or throw.
-     *
-     * @param Request $request
      * @throws AuthorizationException
      */
-    public function authorizeToShow(Request $request)
+    public function authorizeToShow(Request $request): void
     {
         $this->authorizeTo($request, 'show');
     }
 
-    /**
-     * Determine if the current user can view the given resource.
-     *
-     * @param Request $request
-     * @return bool
-     */
-    public function authorizedToShow(Request $request)
+    public function authorizedToShow(Request $request): bool
     {
         return $this->authorizedTo($request, 'show');
     }
 
     /**
-     * Determine if the current user can store new repositories or throw an exception.
-     *
-     * @param  Request  $request
-     * @return void
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws AuthorizationException
      */
-    public static function authorizeToStore(Request $request)
+    public static function authorizeToStore(Request $request): void
     {
         if (! static::authorizedToStore($request)) {
             throw new AuthorizationException('Unauthorized to store.');
         }
     }
 
-    public static function authorizeToStoreBulk(Request $request)
+    /**
+     * @throws AuthorizationException
+     */
+    public static function authorizeToStoreBulk(Request $request): void
     {
         if (! static::authorizedToStoreBulk($request)) {
             throw new AuthorizationException('Unauthorized to store bulk.');
         }
     }
 
-    /**
-     * Determine if the current user can store new repositories.
-     *
-     * @param  Request  $request
-     * @return bool
-     */
-    public static function authorizedToStore(Request $request)
+    public static function authorizedToStore(Request $request): bool
     {
         if (static::authorizable()) {
             return Gate::check('store', static::guessModelClassName());
@@ -120,7 +74,7 @@ trait AuthorizableModels
         return false;
     }
 
-    public static function authorizedToStoreBulk(Request $request)
+    public static function authorizedToStoreBulk(Request $request): bool
     {
         if (static::authorizable()) {
             return Gate::check('storeBulk', static::guessModelClassName());
@@ -130,19 +84,14 @@ trait AuthorizableModels
     }
 
     /**
-     * Determine if the current user can update the given resource or throw an exception.
-     *
-     * @param  Request  $request
-     * @return void
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws AuthorizationException
      */
-    public function authorizeToUpdate(Request $request)
+    public function authorizeToUpdate(Request $request): void
     {
         $this->authorizeTo($request, 'update');
     }
 
-    public function authorizeToAttach(Request $request, $method, $model)
+    public function authorizeToAttach(Request $request, $method, $model): bool
     {
         if (! static::authorizable()) {
             return false;
@@ -164,19 +113,22 @@ trait AuthorizableModels
     public function authorizeToDetach(Request $request, $method, $model)
     {
         if (! static::authorizable()) {
-            return false;
+            throw new AuthorizationException();
         }
 
         $authorized = method_exists(Gate::getPolicyFor($this->model()), $method)
             ? Gate::check($method, [$this->model(), $model])
-            : true;
+            : false;
 
         if (false === $authorized) {
             throw new AuthorizationException();
         }
     }
 
-    public function authorizeToUpdateBulk(Request $request)
+    /**
+     * @throws AuthorizationException
+     */
+    public function authorizeToUpdateBulk(Request $request): void
     {
         $this->authorizeTo($request, 'updateBulk');
     }
@@ -186,75 +138,40 @@ trait AuthorizableModels
         $this->authorizeTo($request, 'deleteBulk');
     }
 
-    /**
-     * Determine if the current user can update the given resource.
-     *
-     * @param  Request  $request
-     * @return bool
-     */
-    public function authorizedToUpdate(Request $request)
+    public function authorizedToUpdate(Request $request): bool
     {
         return $this->authorizedTo($request, 'update');
     }
 
     /**
-     * Determine if the current user can delete the given resource or throw an exception.
-     *
-     * @param  Request  $request
-     * @return void
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws AuthorizationException
      */
-    public function authorizeToDelete(Request $request)
+    public function authorizeToDelete(Request $request): void
     {
         $this->authorizeTo($request, 'delete');
     }
 
-    /**
-     * Determine if the current user can delete the given resource.
-     *
-     * @param  Request  $request
-     * @return bool
-     */
-    public function authorizedToDelete(Request $request)
+    public function authorizedToDelete(Request $request): bool
     {
         return $this->authorizedTo($request, 'delete');
     }
 
     /**
-     * Determine if the current user has a given ability.
-     *
-     * @param  Request  $request
-     * @param string $ability
-     * @return void
-     *
-     * @throws \Illuminate\Auth\Access\AuthorizationException
+     * @throws AuthorizationException
      */
-    public function authorizeTo(Request $request, $ability): void
+    public function authorizeTo(Request $request, iterable|string $ability): void
     {
         if ($this->authorizedTo($request, $ability) === false) {
             throw new AuthorizationException();
         }
     }
 
-    /**
-     * Determine if the current user can view the given resource.
-     *
-     * @param  Request  $request
-     * @param string $ability
-     * @return bool
-     */
-    public function authorizedTo(Request $request, $ability): bool
+    public function authorizedTo(Request $request, iterable|string $ability): bool
     {
         return static::authorizable() && Gate::check($ability, $this->resource);
     }
 
-    /**
-     * Determine if the trait is used by repository or model.
-     *
-     * @return bool
-     */
-    public static function isRepositoryContext()
+    public static function isRepositoryContext(): bool
     {
         return new static() instanceof Repository;
     }
