d3279286d8487e71882bef5e3bbbed969ba3b70f
binaryk <binaryk@users.noreply.github.com>
2022-07-23 15:30:47 +0000

    Fix styling

--- tests/Repositories/PublicRepositoriesTest.php
+++ tests/Repositories/PublicRepositoriesTest.php
@@ -3,17 +3,17 @@
 use Binaryk\LaravelRestify\Tests\Fixtures\User\UserRepository;
 use function Pest\Laravel\get;
 
-beforeEach(function() {
+beforeEach(function () {
     UserRepository::$public = true;
 
     config()->set('restify.middleware', [
         'auth:sanctum' => function ($request, $next) {
             abort(403);
-        }
+        },
     ]);
 });
 
-it('cannot access repositories publicly', function() {
+it('cannot access repositories publicly', function () {
     $this->logout();
 
     get(UserRepository::route())->assertForbidden();
