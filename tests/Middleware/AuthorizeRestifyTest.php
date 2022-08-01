6b9d8adeb17831c3ee225b77420208bb09c1c1ea
Eduard Lupacescu <eduard.lupacescu@binarcode.com>
2022-08-01 20:50:02 +0300

    fix: wip

--- tests/Middleware/AuthorizeRestifyTest.php
+++ tests/Middleware/AuthorizeRestifyTest.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace Binaryk\LaravelRestify\Tests\Unit;
+namespace Binaryk\LaravelRestify\Tests\Middleware;
 
 use Binaryk\LaravelRestify\Restify;
 use Binaryk\LaravelRestify\Tests\Fixtures\User\UserRepository;
 use Binaryk\LaravelRestify\Tests\IntegrationTest;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthorizeRestifyTest extends IntegrationTest
 {
-    use RefreshDatabase;
-
     public function test_unauthorized_http_code_is_401(): void
     {
-        $this->logout();
-
         Restify::$authUsing = fn() => false;
 
         $this->getJson(UserRepository::route())
             ->assertStatus(401);
+
+        Restify::$authUsing = fn() => true;
+
+        $this->getJson(UserRepository::route())
+            ->assertStatus(200);
     }
 }
