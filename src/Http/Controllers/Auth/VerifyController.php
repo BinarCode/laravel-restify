f75ad50d4c7bd54ac5027c0770b290f19a09ab40
Eduard Lupacescu <eduard.lupacescu@binarcode.com>
2022-12-20 20:36:22 +0200

    fix: fixing dynamic user class

--- src/Http/Controllers/Auth/VerifyController.php
+++ src/Http/Controllers/Auth/VerifyController.php
@@ -3,9 +3,9 @@
 namespace Binaryk\LaravelRestify\Http\Controllers\Auth;
 
 use Binaryk\LaravelRestify\Contracts\Sanctumable;
-use Binaryk\LaravelRestify\Tests\Fixtures\User\User;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Events\Verified;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Routing\Controller;
 
@@ -13,9 +13,12 @@ class VerifyController extends Controller
 {
     public function __invoke(int $id, string $hash)
     {
-        $user = User::query()->findOrFail($id);
+        /**
+         * @var Authenticatable $user
+         */
+        $user = config('restify.auth.user_model')::query()->findOrFail($id);
 
-        if ($user instanceof Sanctumable && ! hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
+        if ($user instanceof Sanctumable && !hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
             throw new AuthorizationException('Invalid hash');
         }
 
