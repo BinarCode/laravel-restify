a903f54a4617c7b0cf73e670c1dc958d18c8cef2
Eduard Lupacescu <eduard.lupacescu@binarcode.com>
2023-02-11 23:10:49 +0200

    fix: hide hidden pivots

--- src/Fields/BelongsToMany.php
+++ src/Fields/BelongsToMany.php
@@ -4,6 +4,7 @@ namespace Binaryk\LaravelRestify\Fields;
 
 use Binaryk\LaravelRestify\Contracts\RestifySearchable;
 use Binaryk\LaravelRestify\Fields\Concerns\Attachable;
+use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Binaryk\LaravelRestify\Repositories\PivotsCollection;
 use Binaryk\LaravelRestify\Repositories\Repository;
 use Closure;
@@ -55,6 +56,7 @@ class BelongsToMany extends EagerField
                     ->withPivots(
                         PivotsCollection::make($this->pivotFields)
                             ->map(fn (Field $field) => clone $field)
+                            ->filter(fn (Field $field) => !$field->isHidden(app(RestifyRequest::class)))
                             ->resolveFromPivot($item->pivot)
                     )
                     ->eager($this);
