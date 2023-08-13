0afade796d9debf07a21ec0bd11efaac1e42605b
Eduard Lupacescu <eduard.lupacescu@binarcode.com>
2023-08-13 15:23:00 +0300

    fix: gtm

--- docs-v2/nuxt.config.js
+++ docs-v2/nuxt.config.js
@@ -1,7 +1,13 @@
 import theme from '@nuxt/content-theme-docs';
 
 export default theme({
+  modules: [
+    '@nuxtjs/gtm',
+  ],
+  gtm: {
+    id: 'G-T3S2WB57KE',
+  },
   docs: {
     primaryColor: '#787af6'
-  }
+  },
 });
