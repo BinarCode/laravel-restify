528b2dc8111e0ccf5d776e8b92754fe5d87c8e81
Lupacescu Eduard <eduard.lupacescu@binarcode.com>
2023-09-07 09:48:08 +0200

    fix: adding natural sort filter (#586)

--- tests/Feature/Filters/SortableFilterTest.php
+++ tests/Feature/Filters/SortableFilterTest.php
@@ -3,6 +3,7 @@
 namespace Binaryk\LaravelRestify\Tests\Feature\Filters;
 
 use Binaryk\LaravelRestify\Filters\SortableFilter;
+use Binaryk\LaravelRestify\Filters\Sorts\NaturalSortFilter;
 use Binaryk\LaravelRestify\Tests\Fixtures\User\User;
 use Binaryk\LaravelRestify\Tests\Fixtures\User\UserRepository;
 use Binaryk\LaravelRestify\Tests\IntegrationTestCase;
@@ -32,5 +33,52 @@ class SortableFilterTest extends IntegrationTestCase
             ->json('data.0.attributes.name'));
         $this->assertSame('Alisa', $this->getJson(UserRepository::route(query: ['sort' => '-name']))
             ->json('data.1.attributes.name'));
+    }
+
+    public function test_can_order_using_natural_sortable_filter(): void
+    {
+        User::factory()->create([
+            'name' => '1',
+        ]);
+
+        User::factory()->create([
+            'name' => '10',
+        ]);
+
+        User::factory()->create([
+            'name' => '2',
+        ]);
+
+        User::factory()->create([
+            'name' => '20',
+        ]);
+
+        UserRepository::$sort = [
+            'name' => NaturalSortFilter::class,
+        ];
+
+        $this->assertSame('1', $this->getJson(UserRepository::route(query: ['sort' => 'name']))
+            ->json('data.0.attributes.name'));
+
+        $this->assertSame('2', $this->getJson(UserRepository::route(query: ['sort' => 'name']))
+            ->json('data.1.attributes.name'));
+
+        $this->assertSame('10', $this->getJson(UserRepository::route(query: ['sort' => 'name']))
+            ->json('data.2.attributes.name'));
+
+        $this->assertSame('20', $this->getJson(UserRepository::route(query: ['sort' => 'name']))
+            ->json('data.3.attributes.name'));
+
+        $this->assertSame('20', $this->getJson(UserRepository::route(query: ['sort' => '-name']))
+            ->json('data.0.attributes.name'));
+
+        $this->assertSame('10', $this->getJson(UserRepository::route(query: ['sort' => '-name']))
+            ->json('data.1.attributes.name'));
+
+        $this->assertSame('2', $this->getJson(UserRepository::route(query: ['sort' => '-name']))
+            ->json('data.2.attributes.name'));
+
+        $this->assertSame('1', $this->getJson(UserRepository::route(query: ['sort' => '-name']))
+            ->json('data.3.attributes.name'));
     }
 }
