1d41e7246c0c46151ac9a2df0d0be8304d08efef
Lupacescu Eduard <eduard.lupacescu@binarcode.com>
2023-12-30 12:55:21 +0000

    restifyjs setup without auth middleware (#598)
    * fix: restifyjs setup without auth middleware
    
    * Fix styling
    
    ---------
    
    Co-authored-by: binaryk <binaryk@users.noreply.github.com>

--- src/Bootstrap/RoutesDefinition.php
+++ src/Bootstrap/RoutesDefinition.php
@@ -6,7 +6,6 @@ use Binaryk\LaravelRestify\Http\Controllers\GlobalSearchController;
 use Binaryk\LaravelRestify\Http\Controllers\ProfileController;
 use Binaryk\LaravelRestify\Http\Controllers\ProfileUpdateController;
 use Binaryk\LaravelRestify\Http\Controllers\RestifyJsSetupController;
-use Binaryk\LaravelRestify\Http\Middleware\RestifySanctumAuthenticate;
 use Illuminate\Support\Facades\Route;
 
 class RoutesDefinition
@@ -169,9 +168,8 @@ class RoutesDefinition
         Route::post('/profile', ProfileUpdateController::class)->name('profile.updatePost');
 
         // RestifyJS
-        Route::get('/restifyjs/setup', RestifyJsSetupController::class)->withoutMiddleware(
-            RestifySanctumAuthenticate::class,
-        )->name('restifyjs.setup');
+        Route::get('/restifyjs/setup',
+            RestifyJsSetupController::class)->withoutMiddleware('auth:sanctum')->name('restifyjs.setup');
     }
 
     public function withoutMiddleware(...$middleware): self
