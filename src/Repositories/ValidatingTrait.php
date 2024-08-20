353863fff2b0d5745f83b9334c595857cf4e9253
Arthur Kirkosa <arthur.kirkosa@me.com>
2024-08-20 14:06:32 +0300

    fix(Bulk Update): Only Validate Data for Current Item in Loop

--- src/Repositories/ValidatingTrait.php
+++ src/Repositories/ValidatingTrait.php
@@ -171,7 +171,7 @@ trait ValidatingTrait
         })->toArray();
 
         return Validator::make(
-            $plainPayload ?? $request->all(),
+            [$plainPayload] ?? $request->all(),
             $on->getUpdatingBulkRules($request),
             $messages
         )->after(function ($validator) use ($request) {
@@ -191,13 +191,21 @@ trait ValidatingTrait
         //
     }
 
-    protected static function afterStoringValidation(RestifyRequest $request, $validator) {}
+    protected static function afterStoringValidation(RestifyRequest $request, $validator)
+    {
+    }
 
-    protected static function afterStoringBulkValidation(RestifyRequest $request, $validator) {}
+    protected static function afterStoringBulkValidation(RestifyRequest $request, $validator)
+    {
+    }
 
-    protected static function afterUpdatingValidation(RestifyRequest $request, $validator) {}
+    protected static function afterUpdatingValidation(RestifyRequest $request, $validator)
+    {
+    }
 
-    protected static function afterUpdatingBulkValidation(RestifyRequest $request, $validator) {}
+    protected static function afterUpdatingBulkValidation(RestifyRequest $request, $validator)
+    {
+    }
 
     /**
      * @return array
