c55cdb2b0e7015896c7e012349ae7abceb9a102c
binaryk <binaryk@users.noreply.github.com>
2025-08-23 09:49:06 +0000

    Fix styling

--- src/Eager/Related.php
+++ src/Eager/Related.php
@@ -5,7 +5,6 @@ namespace Binaryk\LaravelRestify\Eager;
 use Binaryk\LaravelRestify\Fields\EagerField;
 use Binaryk\LaravelRestify\Filters\RelatedQuery;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
-use Binaryk\LaravelRestify\MCP\Requests\McpRequest;
 use Binaryk\LaravelRestify\Repositories\Repository;
 use Binaryk\LaravelRestify\Traits\HasColumns;
 use Binaryk\LaravelRestify\Traits\Make;
