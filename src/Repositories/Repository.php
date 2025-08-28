69d30b281bfb2de274d7799c35e81dce0fdb3d94
binaryk <binaryk@users.noreply.github.com>
2025-08-28 10:00:50 +0000

    Fix styling

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -627,7 +627,7 @@ class Repository implements JsonSerializable, RestifySearchable
         $paginator = RepositorySearchService::make()->search($request, $this)
             ->paginate($request->pagination()->perPage ?? static::$defaultPerPage, page: $request->pagination()->page);
 
-        $items = $this->indexCollection($request, $paginator->getCollection())->map(function ($value) use ($request) {
+        $items = $this->indexCollection($request, $paginator->getCollection())->map(function ($value) {
             $repository = static::resolveWith($value);
             // Ensure each resolved repository maintains the original request
 
