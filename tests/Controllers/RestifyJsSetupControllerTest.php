03033a8542b59375b452c07c17e5b57682b843ae
Eduard Lupacescu <lupacescueduard@gmail.com>
2025-09-26 12:49:05 +0300

    fix: wip

--- tests/Controllers/RestifyJsSetupControllerTest.php
+++ tests/Controllers/RestifyJsSetupControllerTest.php
@@ -9,7 +9,6 @@ class RestifyJsSetupControllerTest extends IntegrationTestCase
 {
     public function test_returns_configurations(): void
     {
-        $this->markTestSkipped('Skipping until we decide how to handle JS setup.');
         $this
             ->withoutExceptionHandling()
             ->getJson(Restify::path('restifyjs/setup'))
