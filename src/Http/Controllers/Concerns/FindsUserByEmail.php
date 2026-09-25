40f5ecb9871e1fc30e28461bda280bc794ee6f05
Arthur Kirkosa <arthur.kirkosa@me.com>
2026-09-25 11:29:01 +0300

    fix: only accept a user whose email matches the given one ignoring case

--- src/Http/Controllers/Concerns/FindsUserByEmail.php
+++ src/Http/Controllers/Concerns/FindsUserByEmail.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Binaryk\LaravelRestify\Http\Controllers\Concerns;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 trait FindsUserByEmail
@@ -23,8 +24,20 @@ trait FindsUserByEmail
             ->whereIn('email', array_values(array_unique([$email, $lowercasedEmail])))
             ->get();
 
-        return $users->firstWhere('email', $email)
-            ?? $users->firstWhere('email', $lowercasedEmail)
-            ?? $users->first();
+        return self::userMatchingEmail($users, $email);
+    }
+
+    /**
+     * @template TUser of Model
+     *
+     * @param  Collection<int, TUser>  $users
+     * @return TUser|null
+     */
+    private static function userMatchingEmail(Collection $users, string $email): ?Model
+    {
+        $lowercasedEmail = Str::lower($email);
+
+        return $users->first(static fn (Model $user): bool => $user->getAttribute('email') === $email)
+            ?? $users->first(static fn (Model $user): bool => \is_string($user->getAttribute('email')) && Str::lower($user->getAttribute('email')) === $lowercasedEmail);
     }
 }
